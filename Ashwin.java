4ba4a7bd89b093109233b47908f2be66b89d4f02
ashwinashok18 <84340517+ashwinashok18@users.noreply.github.com>
2021-10-06 16:16:51 +0530

    Add files via upload

---
+++ Ashwin.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Ashwin
+{
+int sum;
+int []a= new a();
+Scanner sc= new Scanner(System.in);
+System.out.println("enter the number");
+sum=sc.nextInt();
+if(a[]!=(n/2==0))
+{
+sum=a[]+1;
+}
+System.out.println("result is" +sum);
+}
+
+
---
+++ Ashwin2.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+class Aswin
+{
+int a;
+int arr[]= new arr(a);
+Scanner sc = new scanner(System.in);
+System.out.println("enter the limit");
+a=sc.nextInt();
+}
+void read()
+{
+System.out.println("find the lowest");
+lowest=sc.nextInt();
+System.out.println("find the highest");
+highest=sc.nextInt();
+System.out.println("find the average");
+average=sc.nextInt();
+}
+void display()
+{
+System.out.println("lowest is"+ lowest);
+Sysyem.out.println("highest is"+ highest);
+System.out.println("average is"+ average);
+}
+}
+class Ashwin
+{
+
+public static void average()
+{
+
+
+
